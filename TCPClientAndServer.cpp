99830ece18a4c1daab1179674c0e43d8ee6bb99f
29637 <29637@LAPTOP-MBA5B74K>
2023-08-07 09:27:39 +0800

    添加项目文件。

---
+++ TCPClientAndServer.cpp
@@ -0,0 +1,28 @@
+﻿// TCPClientAndServer.cpp: 定义应用程序的入口点。
+//
+#include <iostream>
+#include "CTcpServer.h"
+
+int main()
+{
+    {
+        CTcpServer tcpServer("127.0.0.1", 6005);
+
+        if (!tcpServer.InitServer())
+            getchar();
+
+        for (int i = 0; i != 3; ++i)
+        {
+            //接收数据成功后，向客户端返回"answer"
+            if (tcpServer.RecvMsg())
+            {
+                std::string strAnswerMsg{ "answer" };
+
+                tcpServer.SendMsg(strAnswerMsg);
+            }
+        }
+    }//接收3次数据后出此大括号，tcpServer对象被析构，客户端连接被关闭
+
+    return 0;
+}
+
